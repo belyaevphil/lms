23918ba0b8dc5f554fa8237614921c168b82e339
Phil Belyaev <belyaevfil@gmail.com>
2020-07-19 10:31:05 +0500

    Added new user route to get profile

--- server/routers/usersRouter.js
+++ server/routers/usersRouter.js
@@ -9,6 +9,7 @@ const router = express.Router()
 
 router.get('/get', pagination.paginatedResults(User), controller.getUsers) //    example: /api/users/get?page=1
 router.get('/profiles', controller.getUserById) //    example: /api/users/profiles?id=1
+router.get('/myprofile', permission.canViewAndChangeOwnInfo, controller.getMyProfile)
 router.get('/logout', controller.logoutUser)
 router.get('/checkloginstatus', permission.canViewAndChangeOwnInfo, controller.checkloginstatus)
 router.post('/register', validation.registerUserValidationData, controller.registerUser)
