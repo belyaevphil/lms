b7b1eaf63e689eea3bb17bd982c810e3e74cd9b4
Phil Belyaev <belyaevfil@gmail.com>
2020-07-23 14:48:48 +0500

    added functionality to App.jsx file

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { LoggedInModerator } from './components/Content/PagesForLoggedInModerator/LoggedInModerator'
+import { LoggedInUser } from './components/Content/PagesForLoggedInUser/LoggedInUser'
+import { LoggedOut } from './components/Content/PagesForLoggedOut/LoggedOut'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      loginStatus: false,
+      role: null
+    }
+  }
+
+  async checkLoginStatus() {
+    try {
+      const response = await fetch('http://localhost:5000/api/users/checkloginstatus', {
+        method: 'GET',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      const data = await response.json()
+      if (!response.ok && this.state.loginStatus) window.location.reload()
+      const { loginStatus, role } = data
+      if (loginStatus && !this.state.loginStatus) {
+        this.setState({
+          loginStatus: true,
+          role
+        })
+      } else if (!loginStatus) {
+        this.setState({
+          loginStatus: false,
+          role: null
+        })
+      }
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  loginHandler = role => {
+    this.setState({
+      loginStatus: true,
+      role
+    })
+  }
+
+  logoutHandler = () => {
+    this.setState({
+      loginStatus: false,
+      role: null
+    })
+  }
+
+  componentDidMount() {
+    this.checkLoginStatus()
+  }
+
+  render() {
+    const { loginStatus, role } = this.state
+
+    if (loginStatus && role === 'Пользователь') {
+      return <LoggedInUser logoutHandler={this.logoutHandler} />
+    } else if (loginStatus && role === 'Модератор') {
+      return <LoggedInModerator logoutHandler={this.logoutHandler} />
+    } else {
+      return <LoggedOut loginHandler={this.loginHandler} />
+    }
+  }
 }
-
-export default App;
