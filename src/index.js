2754371fd1f866cbacfb36b40dd15833f1c4a0f9
Phil Belyaev <belyaevfil@gmail.com>
2020-07-24 11:18:12 +0500

    added browser router to wrap main app component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import * as serviceWorker from './serviceWorker'
+import { BrowserRouter } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+
+import App from './App'
+
+import 'react-toastify/dist/ReactToastify.css'
+
+import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+      <ToastContainer />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
