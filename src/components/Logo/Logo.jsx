d022ea98aacf28ac5ccd73f485bee78401e7bb2f
Phil Belyaev <belyaevfil@gmail.com>
2020-07-25 11:55:57 +0500

    created logo

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './Logo.css'
+
+export const Logo = () => {
+    return (
+        <div className="logo">
+            <div className="logo-wrapper">Лингвистическая школа</div>
+        </div>
+    )
+}
