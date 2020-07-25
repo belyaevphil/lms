cc4f638920b5e0fce727c12ea517b56ff979cc10
Phil Belyaev <belyaevfil@gmail.com>
2020-07-25 11:59:12 +0500

    created footer and styles for it

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './Footer.css'
+
+export const Footer = () => {
+    return (
+        <div id="footer">
+            <div className="footer-wrapper">Футер</div>
+        </div>
+    )
+}
