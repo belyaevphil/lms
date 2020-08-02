6c51f72ddb6bc08cf8df9d25c98cef251185625e
Phil Belyaev <belyaevfil@gmail.com>
2020-08-02 18:35:39 +0500

    created search block container

---
+++ src/components/Content/MyCourses/SearchBlock/SearchBlockContainer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import SearchBlock from './SearchBlock';
+
+export default class SearchBlockContainer extends React.Component {
+    render() {
+        return (
+            <SearchBlock />
+        );
+    }
+}
