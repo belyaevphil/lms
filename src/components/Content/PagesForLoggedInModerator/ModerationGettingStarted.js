0479d0bcc61aebe6e1d2d022868ca421151d613f
Phil Belyaev <belyaevfil@gmail.com>
2020-08-12 16:34:54 +0500

    created ModerationGettingStarted

---
+++ src/components/Content/PagesForLoggedInModerator/ModerationGettingStarted.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const ModerationGettingStarted = () => {
+    return (
+        <div className="moderationContent-wrapper">
+            <div className="moderationContent">
+                <h2>Добро пожаловать в панель управления</h2>
+            </div>
+        </div>
+    )
+}
