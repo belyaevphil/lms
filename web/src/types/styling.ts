961eddc03b3f0d1d3fbc7b8aa0833dd12e8edf9a
Phil Belyaev <belyaevfil@gmail.com>
2021-04-04 15:15:18 +0500

    added types for styling components

---
+++ web/src/types/styling.ts
@@ -0,0 +1,8 @@
+export type JustifyContent =
+  | 'flex-end'
+  | 'center'
+  | 'space-between'
+  | 'space-around'
+  | 'space-evenly'
+  | 'flex-start'
+export type AlignItems = 'flex-start' | 'flex-end' | 'center' | 'baseline'
