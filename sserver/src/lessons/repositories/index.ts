2fbcd43f251c619ff906b55979ca5d46a80c7623
Phil Belyaev <belyaevfil@gmail.com>
2021-05-21 19:11:59 +0500

    added index file for lessons repositories

---
+++ sserver/src/lessons/repositories/index.ts
@@ -0,0 +1,3 @@
+export { LessonsRepository } from './lessons.repository'
+export { StudentsLessonsRepository } from './students-lessons.repository'
+export { LessonsFilesRepository } from './lessons-files.repository'
