f6cb747f5f5e8b1a4c3aaf4be865458360f06bb8
Phil Belyaev <belyaevfil@gmail.com>
2021-06-01 16:46:11 +0500

    added index file for user repositories

---
+++ sserver/src/users/repositories/index.ts
@@ -0,0 +1,4 @@
+export { InstructorsRepository } from './instructors.repository'
+export { RolesRepository } from './roles.repository'
+export { UsersRolesRepository } from './users-roles.repository'
+export { UsersRepository } from './users.repository'
